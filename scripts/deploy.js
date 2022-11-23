42b415d6e5721369c04575df1687591f0a5d4247
jayjeet kumar <jayjeet1013@gmail.com>
2022-11-23 20:59:37 +0530

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,18 +7,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const Opensource = await hre.ethers.getContractFactory("Opensource");
+  const opensource = await Opensource.deploy(1000,3600);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await opensource.deployed();
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("Address is :", opensource.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
